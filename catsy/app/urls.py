2c750c9fc28e1bb2ec0429cba9034065664a34f4
Flavio Manyari <flaviomanyariz@gmail.com>
2023-06-07 20:37:05 -0500

    Redireccionar al home cuando cierra sesión

--- catsy/app/urls.py
+++ catsy/app/urls.py
@@ -35,7 +35,7 @@ urlpatterns = [
     path("accounts/login/", auth_view.LoginView.as_view(template_name='app/login.html', authentication_form=LoginForm), name="login"),
     path('passwordchange/', auth_view.PasswordChangeView.as_view(template_name='app/changepassword.html', form_class=MyPasswordChangeForm, success_url='/passwordchangedone'), name='passwordchange'),
     path('passwordchangedone/', auth_view.PasswordChangeDoneView.as_view(template_name='app/passwordchangedone.html'), name='passwordchangedone'),
-    path("logout/", auth_view.LogoutView.as_view(next_page='login'), name="logout"),
+    path("logout/", auth_view.LogoutView.as_view(next_page='/'), name="logout"),
 
     # para el reseteo de contraseña
     path("password-reset/", auth_view.PasswordResetView.as_view(template_name='app/password_reset.html', form_class=MyPasswordResetForm), name="password_reset"),
